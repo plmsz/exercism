5fb5e2efd0fda4467057f3708da4675508306e59
paloma.souza <paloma.souza@ambevtech.com.br>
2021-08-23 17:52:10 -0300

    JS_triangle

--- teams/start-tech/javascript/triangle/triangle.js
+++ teams/start-tech/javascript/triangle/triangle.js
@@ -2,21 +2,49 @@
 // This is only a SKELETON file for the 'Triangle' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-
 export class Triangle {
-  constructor(...sides) {
-    throw new Error('Remove this statement and implement this function');
+  sidesA = 0;
+  sidesB = 0;
+  sidesC = 0;
+
+  constructor(...sides) { //spread operator - operações com dados
+    this.sideA = sides[0];
+    this.sideB = sides[1];
+    this.sideC = sides[2];
+    this.sides = sides;
+  }
+  
+  get isAllSidesGreaterThan0(){
+    return this.sideA > 0 && this.sideB > 0 && this.sideC > 0;
+  }
+
+  get isSumOfLenghtsGreaterThanThirdSide(){
+   return this.sideA + this.sideB >= this.sideC && this.sideA + this.sideC >= this.sideB && this.sideB + this.sideC >= this.sideA;
+  }
+  
+  get isExistent(){
+    return this.isAllSidesGreaterThan0 && this.isSumOfLenghtsGreaterThanThirdSide;
   }
 
   get isEquilateral() {
-    throw new Error('Remove this statement and implement this function');
+    return this.isExistent && this.sideA == this.sideB && this.sideB == this.sideC;
   }
-
   get isIsosceles() {
-    throw new Error('Remove this statement and implement this function');
+    if (this.isExistent) {
+      return this.sideA == this.sideB || this.sideA == this.sideC || this.sideB == this.sideC;
+    } else {
+      return false;
+    }
   }
 
   get isScalene() {
-    throw new Error('Remove this statement and implement this function');
+    if (this.isExistent) {
+      return this.sideA != this.sideB && this.sideA != this.sideC && this.sideB != this.sideC
+    } else {
+      return false;
+    }
+  }
+  get isDegenerate() {
+    return this.sideA + this.sideB == this.sideC || this.sideA + this.sideC == this.sideB || this.sideB + this.sideC == this.sideA;
   }
 }
