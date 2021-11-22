59f85abc80f4ea150a603d4c5bf3633b9a487264
paloma.souza <paloma.souza@ambevtech.com.br>
2021-11-22 08:15:35 -0300

    rna_ts

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,23 +1,20 @@
-enum DnaBasis {
-  G = "C",
-  C = "G",
-  T = "A",
-  A = "U",
-}
-type DnaBase = keyof typeof DnaBasis; //criando um tipo que aceita so as letras do enum
-export function toRna(dna: string): string {
-  let rnaResultado = ""; //criando uma variavel pra colocar os resultados
+/*Refazer*/
+// enum DnaBasis {
+//   G = "C",
+//   C = "G",
+//   T = "A",
+//   A = "U",
+// }
+// type KeyDna = keyof typeof DnaBasis; //criando um tipo que aceita so as letras do enum
+// export function toRna(dna: string): string {
+//   let rnaResultado = ""; //criando uma variavel pra colocar os resultados
 
-  for (const elemento of dna) {
-    /*varrendo a string letra por letra*/
-    if (!(elemento in DnaBasis))
-      throw Error(
-        "Invalid input DNA."
-      ); /*testando se alguma letra nao é correspondente ao enum e enviando um erro */
-    rnaResultado +=
-      DnaBasis[
-        <DnaBase>elemento
-      ]; /*somando cada elemento no array rnaResultado e transformando a variavel no tipo dentro da tag */
-  }
-  return rnaResultado;
-}
+//   /*varrendo a string letra por letra*/
+//   for (const elemento of dna) {
+//     /*testando se alguma letra nao é correspondente ao enum e enviando um erro */
+//     if (!(elemento in DnaBasis)) throw Error("Invalid input DNA.");
+//     rnaResultado += DnaBasis[<KeyDna>elemento];
+//   }
+//   /*somando cada elemento no array rnaResultado e transformando a variavel no tipo dentro da tag, elemento é uma string para acessar as chaves de DnaBasis usa-se as tags para transforma-la do tipo DnaBasis */
+//   return rnaResultado;
+// }
