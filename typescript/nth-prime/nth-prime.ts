230abffe3b7adf6ec0d2166246bd9f73b4f16de5
paloma.souza <paloma.souza@ambevtech.com.br>
2021-11-22 08:19:16 -0300

    nth-prime

---
+++ typescript/nth-prime/nth-prime.ts
@@ -0,0 +1,25 @@
+/*Refazer*/ 
+
+// function *generatePrimes(limit: number): Iterable<number> {
+//   var maybePrime = 3;
+//   var isPrime:number[] = [2];
+//   if (limit >= 1) {
+//     yield 2;
+//   }
+//   while (isPrime.length < limit) {
+//     if (isPrime.every(value => maybePrime % value != 0)) {
+//       isPrime.push(maybePrime);
+//       yield maybePrime;
+//     }
+//       maybePrime += 2;
+//   }
+// }
+// export function nth(numero: number): number {
+//   const primo =  Array.from(generatePrimes(numero)).pop();
+//   if (!primo) {
+//     throw new Error ('Prime is not possible')
+//   } else {
+//     return primo;
+//   }
+// }
+
