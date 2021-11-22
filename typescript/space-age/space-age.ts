e24831dca0d88a3478549719d88ba986e3a3f775
paloma.souza <paloma.souza@ambevtech.com.br>
2021-11-22 08:15:02 -0300

    space_age_ts

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,17 +1,18 @@
-type Planet = keyof typeof Planets; //criando o próprio tipo
+/*Refazer*/
+// type Planet = keyof typeof Planets; //criando o próprio tipo
 
-export function age(planet: Planet, seconds: number): number {
-  return parseFloat((seconds / Planets[planet]).toFixed(2));
-  // return +((seconds/Planets[planet]).toFixed(2))
-}
+// export function age(planet: Planet, seconds: number): number {
+//   return parseFloat((seconds / Planets[planet]).toFixed(2));
+//   // return +((seconds/Planets[planet]).toFixed(2))
+// }
 
-enum Planets {
-  earth = 31557600, // seconds per year
-  mercury = 0.2408467 * earth,
-  venus = 0.61519726 * earth,
-  mars = 1.8808158 * earth,
-  jupiter = 11.862615 * earth,
-  saturn = 29.447498 * earth,
-  uranus = 84.016846 * earth,
-  neptune = 164.79132 * earth,
-}
+// enum Planets {
+//   earth = 31557600, // seconds per year
+//   mercury = 0.2408467 * earth,
+//   venus = 0.61519726 * earth,
+//   mars = 1.8808158 * earth,
+//   jupiter = 11.862615 * earth,
+//   saturn = 29.447498 * earth,
+//   uranus = 84.016846 * earth,
+//   neptune = 164.79132 * earth,
+// }
